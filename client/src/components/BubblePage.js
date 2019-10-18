9f10e75a66614566e5dd993fda8e8d15530b902e
Christopher Bailey <seabayley@outlook.com>
2019-10-18 11:22:51 -0500

    Added login and bubble private route

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from 'axios';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('http://localhost:5000/api/colors')
+      .then(res => {
+        setColorList(res.data)
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+  })
+
+  if (colorList === []) {
+    return <p> Loading...</p>
+  }
 
   return (
     <>
