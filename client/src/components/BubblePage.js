8a0ceb5f1574c7019ac38428716f6b6f03a14361
Chris Bailey <seabayley@outlook.com>
2019-11-15 11:06:40 -0600

    Updated server auth and added dependency array to bubblepage

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -17,7 +17,7 @@ const BubblePage = () => {
         setColorList(res.data)
       })
       .catch(err => console.log(err))
-  })
+  },[])
 
   if (colorList === []) {
     return <p> Loading...</p>
